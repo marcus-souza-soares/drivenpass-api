908f189deb8ed6e84f8a03bf0cb7f9394991f07e
Marcus <marcusadrenaline@gmail.com>
2022-09-08 09:04:16 -0300

    initials configs

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import chalk from 'chalk';
+import './config/index.js';
+import { errorHandlingMiddleware } from "./middlewares/errorMiddleware.js"
+import "express-async-errors"
+
+import router from './routes/index.js';
+
+const app = express();
+app.use(json());
+app.use(cors());
+app.use(router);
+app.use(errorHandlingMiddleware)
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(chalk.green.bold(`\nServer running on port ${port}...`));
+});
